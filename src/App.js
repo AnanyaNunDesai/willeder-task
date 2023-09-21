d729e2a4cbb3f5c9c86959ff304137f758c06120
AnanyaNunDesai <ananyadesai290402@gmail.com>
2023-09-21 23:50:50 +0530

    Modified carousel to top view height only, added services section

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="home-page">
       <Navbar />
       <Carousel />
-      <div className="intro">
+      <div className="section intro">
         <div className="logo">
           <img src="./images/logo-icon.png" alt="Icon" />
           <h1 className="text">LOGO</h1>
@@ -25,6 +25,36 @@ function App() {
           <p>scroll</p>
           <div className="indicator"></div>
         </div>
+      </div>
+      <div className="section services">
+        <div className="heading">
+          <p>Lorem</p>
+          <p>subtitle</p>
+        </div>
+        <div className="details">
+          <div className="image-example first">
+            <div className="text-example">
+              <p className="web-enum">01</p>
+              <p>Lorem ipsum dolor sit</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+            </div>
+          </div>
+          <div className="image-example second">
+            <div className="text-example">
+              <p className="web-enum">01</p>
+              <p>Lorem ipsum dolor sit</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+            </div>
+          </div>
+          <div className="image-example third">
+            <div className="text-example">
+              <p className="web-enum">01</p>
+              <p>Lorem ipsum dolor sit</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+            </div>
+          </div>
+        </div>
+        <div className="path-bar" />
       </div>
     </div>
   );
