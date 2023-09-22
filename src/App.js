68b18e209099c1d29d7d747f3429c80ab9ba8eaa
AnanyaNunDesai <ananyadesai290402@gmail.com>
2023-09-22 11:54:24 +0530

    Added section 3, cleanup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
+import React, { useEffect, useState } from "react";
 import "./App.scss";
 import Carousel from "./components/Carousel";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkIsMobile = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    checkIsMobile();
+    window.addEventListener("resize", checkIsMobile);
+
+    return () => {
+      window.removeEventListener("resize", checkIsMobile);
+    };
+  }, []);
+
+  // Arranges subsection elements of "See More" if on mobile or web
+  function arrangeSeeMoreSubsection() {
+    // Web view
+    if (!isMobile) {
+      return (
+        <div
+          className="explain-subsection
+          "
+        >
+          <img
+            className="explain-img"
+            src="./images/see-more.png"
+            alt="See More"
+          />
+          <div className="explain-text">
+            <div className="title-underline">
+              <p>Lorem ipsum</p>
+              <div className="underline-bar" />
+            </div>
+            <p className="explain-details">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam,quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <div className="click-me">
+              <p>Lorem ipsum</p>
+              <img src="./images/next-arrow.png" alt="Click Me" />
+            </div>
+          </div>
+        </div>
+      );
+    }
+    // Mobile view, underlined title is above image
+    else {
+      return (
+        <div
+          className="explain-subsection
+        "
+        >
+          <div>
+            <div className="title-underline">
+              <p className="title-text">Lorem ipsum</p>
+              <div className="underline-bar" />
+            </div>
+            <img
+              className="explain-img"
+              src="./images/see-more.png"
+              alt="See More"
+            />
+          </div>
+          <div className="explain-text">
+            <p className="explain-details">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam,quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <div className="click-me">
+              <p>Lorem ipsum</p>
+              <img src="./images/next-arrow.png" alt="Click Me" />
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+
   return (
     <div className="home-page">
       <Navbar />
       <Carousel />
       <div className="section intro">
-        <div className="logo">
-          <img src="./images/logo-icon.png" alt="Icon" />
-          <h1 className="text">LOGO</h1>
-        </div>
-        <div className="subtitle">
-          <p>Lorem ipsum dolor sit amet</p>
-        </div>
-        <div className="description">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
+        <div className="about">
+          <div className="logo">
+            <img src="./images/logo-icon.png" alt="Icon" />
+            <h1 className="text">LOGO</h1>
+          </div>
+          <div className="subtitle">
+            <p>Lorem ipsum dolor sit amet</p>
+          </div>
+          <div className="description">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
         </div>
         <div className="invite-scroll">
           <p>scroll</p>
@@ -54,6 +140,10 @@ function App() {
             </div>
           </div>
         </div>
+        <div className="path-bar" />
+      </div>
+      <div className="section see-more">
+        {arrangeSeeMoreSubsection()}
         <div className="path-bar" />
       </div>
     </div>
